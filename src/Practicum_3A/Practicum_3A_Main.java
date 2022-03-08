a02611c1a0ca9e922906d450b6aad75e67a67bde
sveng <sven.grasmeijr@student.hu.nl>
2022-03-08 14:02:31 +0100

    Repository added

---
+++ src/Practicum_3A/Practicum_3A_Main.java
@@ -0,0 +1,21 @@
+package Practicum_3A;
+
+public class Practicum_3A_Main {
+    public static void main(String[] args) {
+        Voetbalclub ajx = new Voetbalclub("Ajax      ");
+        Voetbalclub feij = new Voetbalclub("Feijenoord");
+
+        feij.verwerkResultaat('w');
+        feij.verwerkResultaat('w');
+        feij.verwerkResultaat('w');
+        feij.verwerkResultaat('g');
+
+        System.out.println("Feijenoord punten: " + feij.aantalPunten());
+        System.out.println("Ajax gespeeld: " + ajx.aantalGespeeld());
+        System.out.println();
+
+        System.out.println(ajx);
+        System.out.println(feij);
+    }
+
+}
---
+++ src/Practicum_3A/Voetbalclub.java
@@ -0,0 +1,38 @@
+package Practicum_3A;
+
+public class Voetbalclub {
+
+    private String naam;
+    private int aantalGewonnen;
+    private int aantalGelijk;
+    private int aantalVerloren;
+
+    public Voetbalclub(String enteredName){
+        naam = enteredName;
+        if (enteredName == null | enteredName == "")
+        {
+            naam = "FC";
+        }
+    }
+
+    public void verwerkResultaat(char ch){
+        if (ch == 'w')
+            aantalGewonnen = aantalGewonnen + 1;
+        if (ch == 'g')
+            aantalGelijk = aantalGelijk + 1;
+        if (ch == 'v')
+            aantalVerloren = aantalVerloren + 1;
+    }
+
+    public int aantalPunten(){
+        return aantalGewonnen * 3 + aantalGelijk;
+    }
+
+    public int aantalGespeeld(){
+        return aantalGewonnen + aantalGelijk + aantalVerloren;
+    }
+
+    public String toString(){
+        return naam+" "+aantalGespeeld()+" "+aantalGewonnen+" "+aantalGelijk+" "+aantalVerloren+" "+aantalPunten();
+    }
+}
